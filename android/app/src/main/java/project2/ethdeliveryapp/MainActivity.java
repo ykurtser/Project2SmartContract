e9d14a139d6d1f57389dc138c088929ee7eccb71
ykurtser <30568424+ykurtser@users.noreply.github.com>
2018-06-27 15:28:24 +0300

    added the basic anroid studio project

---
+++ android/app/src/main/java/project2/ethdeliveryapp/MainActivity.java
@@ -0,0 +1,21 @@
+package project2.ethdeliveryapp;
+package org.web3j;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Web3j web3 = Web3j.build(new HttpService("https://morden.infura.io/your-token"));
+        setContentView(R.layout.activity_main);
+        TextView textView = (TextView) findViewById(R.id.TEXTV1);
+        textView.setText("text you want to display");
+
+    }
+}
