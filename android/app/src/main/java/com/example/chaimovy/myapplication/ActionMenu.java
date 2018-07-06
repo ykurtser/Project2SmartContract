33be64a047ee021c35c54c7d98f9ed92f6259963
chaimovy <chaimovy@gmail.com>
2018-07-06 16:24:58 +0300

    ui to create package

--- android/app/src/main/java/com/example/chaimovy/myapplication/ActionMenu.java
+++ android/app/src/main/java/com/example/chaimovy/myapplication/ActionMenu.java
@@ -1,14 +1,41 @@
 package com.example.chaimovy.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ActionMenu extends AppCompatActivity {
 
+
+    String address;
+    String key;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_action_menu);
+        Button createPkgBt = findViewById(R.id.createPkgBt);
+        if (getIntent().hasExtra("address") &&getIntent().hasExtra("key"))
+        {
+            address=getIntent().getExtras().getString("address");
+            key=getIntent().getExtras().getString("key");
+        }
+
+        createPkgBt.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                Intent I = new Intent(getApplicationContext(), CreatePackage.class);
+                I.putExtra("address", address);
+                I.putExtra("key", key);
+                startActivity(I);
+
+            }
+
+        });
+
     }
 }
--- android/app/src/main/java/com/example/chaimovy/myapplication/MainActivity.java
+++ android/app/src/main/java/com/example/chaimovy/myapplication/MainActivity.java
@@ -1,22 +1,13 @@
 package com.example.chaimovy.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.Button;
 
-import org.web3j.crypto.Credentials;
 import org.web3j.protocol.Web3j;
 import org.web3j.protocol.Web3jFactory;
-import org.web3j.protocol.core.DefaultBlockParameterName;
-import org.web3j.protocol.core.methods.response.EthGetBalance;
-import org.web3j.protocol.core.methods.response.TransactionReceipt;
-import org.web3j.protocol.core.methods.response.Web3ClientVersion;
 import org.web3j.protocol.http.HttpService;
-import org.web3j.tx.Transfer;
-import org.web3j.utils.Convert;
-
-import java.math.BigDecimal;
-import java.math.BigInteger;
-import java.util.concurrent.ExecutionException;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -27,55 +18,39 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Web3j web3 = Web3jFactory.build(new HttpService("http://10.0.2.2:7545"));
-        TextView displayText = findViewById(R.id.textView);
-        try {
-            Web3ClientVersion web3ClientVersion = web3.web3ClientVersion().sendAsync().get();
-            String clientVersion = web3ClientVersion.getWeb3ClientVersion();
-            // send asynchronous requests to get balance
-            EthGetBalance ethGetBalance = web3
-                    .ethGetBalance("0xd51d1b97A1Dab01E0eEd629A31B09D58E3ccB642", DefaultBlockParameterName.LATEST)
-                    .sendAsync()
-                    .get();
+        Button BuyerSellerBt = findViewById(R.id.BuyerSellerBt);
+        Button DeliveryManagerBt = findViewById(R.id.DeliveryManagerBt);
+        Button DeliveryGuyBt = findViewById(R.id.DeliveryGuyBt);
+        BuyerSellerBt.setOnClickListener(new View.OnClickListener(){
 
-            BigInteger wei = ethGetBalance.getBalance();
+            @Override
+            public void onClick(View v){
+                Intent I = new Intent(getApplicationContext(), LoginActivityBuyerSeller.class);
+                startActivity(I);
 
-            //displayText.setText(wei.toString());
-            try
-            {
-            String account = web3.ethAccounts().sendAsync().get().getAccounts().get(0);
-
-            Credentials credentials = Credentials.create(account);
-
-                TransactionReceipt transactionReceipt = Transfer.sendFunds(
-                        web3, credentials, "0xA895A52FD9b319559c17b6FeE7Cbcd10A7efC994", BigDecimal.valueOf(1.0), Convert.Unit.ETHER).send();
-                displayText.setText(transactionReceipt.toString());
-            }
-            /*
-            catch(TransactionException e)
-            {
-                displayText.setText(e.toString()+"TransactionException");
             }
 
-            catch(IOException e)
-            {
-                displayText.setText(e.toString()+"IOexception");
-            }
-            */
-            catch(Exception e)
-            {
-                displayText.setText(e.toString()+" exception");
+        });
+        DeliveryManagerBt.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v){
+                Intent I = new Intent(getApplicationContext(), LoginActivityDeliveryCompany.class);
+                startActivity(I);
+
             }
 
+        });
+        DeliveryGuyBt.setOnClickListener(new View.OnClickListener(){
 
-        }
-        catch(ExecutionException e)
-        {
-            displayText.setText(e.toString()+"excecutionException");
-        }
-        catch(InterruptedException e)
-        {
-            displayText.setText(e.toString()+"intrupptedExeption");
-        }
+            @Override
+            public void onClick(View v){
+                Intent I = new Intent(getApplicationContext(), LoginActivityDeliveryCompany.class);
+                startActivity(I);
+
+            }
+
+        });
 
 
     }
